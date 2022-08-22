9b266310b1e554e110c7269ef4436ebcb18cec64
Ayush Raj Vaish <ayushrajvaishlko78438@gmail.com>
2022-08-22 18:31:19 +0530

    Frontend Changes

--- ServiceBookingPortal/src/app/app.module.ts
+++ ServiceBookingPortal/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthService } from './authentication/auth.service';
@@ -13,15 +12,13 @@ import { UsersModule } from './users/users.module';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { ProfileComponent } from './profile/profile.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AboutUsComponent } from './about-us/about-us.component';
 import { BookingModule } from './booking/booking.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ProfileComponent,
-    AboutUsComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
